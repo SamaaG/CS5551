7aedfbf4bb2c80e1cbc1686922362896a51299e3
SamaaGazzaz <sora.jp@hotmail.Com>
2015-09-24 16:08:30 -0500

    Tutorial 4 is added
    Mashup page added and documentation included

---
+++ Source/Tutorial3 - website/Tutorial3 - website/Controllers/MashupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tutorial3___website.Controllers
+{
+    public class MashupController : Controller
+    {
+        // GET: Mashup
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
