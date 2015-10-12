438b96428a657e73f3f2a27078acacc67cbfc267
SamaaGazzaz <sora.jp@hotmail.Com>
2015-10-12 16:22:44 -0500

    Tutorial 7 is added
    Register, login and saving locations into mongodb is added

--- Source/ASWEHW5/www/js/app.js
+++ Source/ASWEHW5/www/js/app.js
@@ -1,28 +1,23 @@
 angular.module('starter', ['ionic','ngCordova'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();} });    })
-.controller('LocCtrl', function($scope, $ionicModal, $cordovaToast){
+.controller('AppCtrl', function($scope, $ionicModal, $cordovaToast, $http, $httpParamSerializerJQLike){
   
-    $scope.locs= [
-        {title: 'Home',
-         address: '628 Scott ave, Kansas City, MO',
-         img: 'img/home.png'
-        },
-        {title: 'Work',
-         address: '112 Wornall, Overland Park, KS',
-         img: 'img/work.png'
-        },
-        {title: 'Anna\'s house',
-         address: '78 E Santa Fe, Kansas City, MO',
-         img: 'img/anna.png'
-        } ];
+ var loggedIn = false;
+    if (loggedIn){
+   function showLocs(){     
+var locReq = new XMLHttpRequest();
+locReq.open("GET", "https://api.mongolab.com/api/1/databases/asedemo/collections/locs?&apiKey=T3Xt8EWGKBIuFZL_iVclk3noEhpVVU9y", false);
+locReq.send(null); 
+$scope.locs = JSON.parse(locReq.responseText);
+   }
+    };
+//$scope.pageClass = 'register';
     
 $ionicModal.fromTemplateUrl('views/newLoc.html', function (modal){
  $scope.locModal = modal;
@@ -30,7 +25,6 @@ $ionicModal.fromTemplateUrl('views/newLoc.html', function (modal){
    scope: $scope
 });
     
-
 $scope.showToast = function(message, duration, location) {
         $cordovaToast.show(message, duration, location).then(function(success) {
             console.log("The toast was shown");
@@ -39,18 +33,21 @@ $scope.showToast = function(message, duration, location) {
         });
     }
 
-
-$scope.createLoc = function (loc) {
-    $scope.locs.push({
-        title: loc.title,
-        address: loc.address,
-        img: loc.img
-    });
+$scope.createLoc = function (newTitle, newAddress, newImage) {
+    $http({
+    method: 'POST',
+    url : 'https://api.mongolab.com/api/1/databases/asedemo/collections/locs?apiKey=T3Xt8EWGKBIuFZL_iVclk3noEhpVVU9y',
+    data: JSON.stringify({
+                title: newTitle,
+                address: newAddress,
+                img: newImage
+            }),
+    contentType: "application/json"
+})
+    document.getElementById("locForm").reset();
+    if(loggedIn){showLocs();}
     $scope.locModal.hide();
     
-    loc.title="";
-    loc.address="";
-    loc.img="";
 };
 
 $scope.newLoc = function(){
@@ -60,4 +57,66 @@ $scope.newLoc = function(){
 $scope.closeNewLoc = function(){
     $scope.locModal.hide();
 };
+    
+ 
+    
+$ionicModal.fromTemplateUrl('views/openLogin.html', function (modal){
+ $scope.loginModal = modal;
+},{
+   scope: $scope
+});
+    
+$scope.openLogin = function(){
+    $scope.loginModal.show();
+};
+
+$scope.closeLogin = function(){
+    $scope.loginModal.hide();
+}; 
+
+$scope.login = function(username, userpassword) {
+    
+    $http.get('https://api.mongolab.com/api/1/databases/asedemo/collections/users?q={"name":"username", "password":"userpassword"}&apiKey=T3Xt8EWGKBIuFZL_iVclk3noEhpVVU9y'
+).then(function successCallback(response){
+        loggedIn =true;
+        showLocs();
+        $scope.name = "Welcome, " + username + "!";
+        $scope.loginModal.hide();
+        document.getElementById("loginForm").reset();
+        
+    }, function errorCallback(response){$scope.loginErr="There was a problem logging in, please try again!"});
+};
+    
+ 
+    
+$ionicModal.fromTemplateUrl('views/openRegister.html', function (modal){
+ $scope.registerModal = modal;
+},{
+   scope: $scope
+});
+    
+$scope.openRegister = function(){
+    $scope.registerModal.show();
+};
+
+$scope.closeRegister = function(){
+    $scope.registerModal.hide();
+}; 
+    
+$scope.register = function(username, password, email) {
+   console.log("inside login function");
+$http({
+    method: 'POST',
+    url : 'https://api.mongolab.com/api/1/databases/asedemo/collections/users?apiKey=T3Xt8EWGKBIuFZL_iVclk3noEhpVVU9y',
+    data: JSON.stringify({
+                name: username,
+                password: password,
+                email: email
+            }),
+    contentType: "application/json"
+})
+document.getElementById("registerForm").reset();
+$scope.registerModal.hide();
+};
+    
 });
